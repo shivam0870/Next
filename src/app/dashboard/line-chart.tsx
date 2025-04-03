2d70671a6f3595a51681831cc7d471a3f676cf1a
shivam0870 <shivamkrsingh4688@gmail.com>
2025-04-03 17:29:13 +0530

    File Colocation

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,10 @@
+function BarChart(){
+    return <h1>This is a bar chart component</h1>
+}
+
+export default function LineChart() {
+    return <h1>Line Chart Page</h1>
+}
+
+//page.tsx makes the rooutes accessible for all the others the file-based routing wont work here that is why we use concpet of 
+// File Colocation
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function LineChart() {
+    return <h1>This is the page for Line Chart</h1>;
+}
